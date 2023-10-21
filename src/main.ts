32129b872c5f15363e0a3a0a6e0e476811324737
nolood <a9514548799@gmail.com>
2023-10-21 18:16:29 +0500

    feat: docker-compose + dockerfile

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Server start on port 3000');
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
